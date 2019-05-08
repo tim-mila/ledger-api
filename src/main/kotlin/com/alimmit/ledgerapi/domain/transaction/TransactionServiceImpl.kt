af5792475829f70c8d40e1865574296907c00820
timmila <tim.mila@7summitsagency.com>
2019-05-07 21:42:55 -0500

    Negate transaction amount if necessary

--- src/main/kotlin/com/alimmit/ledgerapi/domain/transaction/TransactionServiceImpl.kt
+++ src/main/kotlin/com/alimmit/ledgerapi/domain/transaction/TransactionServiceImpl.kt
@@ -4,9 +4,11 @@ import com.alimmit.ledgerapi.domain.account.AccountRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 import java.util.*
 import javax.transaction.Transactional
 import kotlin.NoSuchElementException
+
 
 @Service
 @Transactional
@@ -16,9 +18,11 @@ class TransactionServiceImpl(val transactionRepository: TransactionRepository, v
 
         val account = accountRepository.findById(transactionDto.accountId).orElseThrow { NoSuchElementException("Account not found") }
 
+        val amount = if (transactionDto.amount > BigDecimal("0.00") && Transaction.Type.Debit == transactionDto.type) transactionDto.amount.negate() else transactionDto.amount
+
         val transaction = Transaction()
         transaction.account = account
-        transaction.amount = transactionDto.amount
+        transaction.amount = amount
         transaction.description = transactionDto.description
         transaction.type = transactionDto.type
         transaction.status = Transaction.Status.Pending
